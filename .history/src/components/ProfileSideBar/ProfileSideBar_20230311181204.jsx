cbe6aadd83652827a571c1115a50afa6e869df0a
Tomi Ogunsan <tomiafolayan@gmail.com>
2023-03-11 18:12:32 +0100

    bug fix

---
+++ .history/src/components/ProfileSideBar/ProfileSideBar_20230311181204.jsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import { BsFillPersonFill, BsFillPersonCheckFill } from 'react-icons/bs'
+import { FiSettings } from 'react-icons/fi'
+import {FaListAlt} from 'react-icons/fa'
+
+import { Link } from 'react-scroll'
+
+
+
+export default function ProfileSideBar({listings=[]}) {
+console.log(listings);
+    const sideBar = [
+        {
+            label: <BsFillPersonFill className='bg-[#f5f5f5] rounded-full text-[#C0C0C0] text-3xl'/>,
+           text: 'Profile',
+           link: 'profile',
+           isListing: false
+        },
+        {
+            label: <BsFillPersonCheckFill className='bg-[#f5f5f5] rounded-full text-[#C0C0C0] text-3xl'/>,
+           text: 'Verification',
+           link: 'verification',
+           isListing: false
+        },
+       
+        {
+            label: <FaListAlt className='bg-[#f5f5f5]  rounded-full text-[#C0C0C0] text-3xl'/>,
+           text: 'Listings',
+           link: 'listings',
+           isListing: true
+        },
+    ]
+
+    
+  return (
+ <div className=' shadow-md fixed '>
+    {sideBar.map((item, idx) => {
+
+            return <>
+            
+         {  item.link !== "listings" && 
+          <div  key={idx} className={ `flex  p-8 
+          ${sideBar.length - 1 === idx ? " border-b-0 border": 'border-b-[1px] border'}`}>
+              <Link to={item.link } smooth={true}  
+            duration={1000} activeClass='active' className='flex'>
+            {item.label}
+             <div className='ml-4 text-lg text-[#C0C0C0]'>{ item.text}</div>
+             </Link>
+            
+         </div>  
+    }
+         </>
+        
+       
+
+})}
+ </div>
+  
+    
+    
+   
+  )
+}
