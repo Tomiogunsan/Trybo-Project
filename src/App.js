5cfcdf7a6183dd957a8bb0362329a1bb87552e37
Tomi Ogunsan <tomiafolayan@gmail.com>
2023-07-29 16:27:59 +0100

    adjusted the ui

--- src/App.js
+++ src/App.js
@@ -1,74 +1,62 @@
-import React, { useState , lazy} from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-const Help = lazy(() => import("./Pages/Help"));
-const Home = lazy(() => import ('./Pages/Home'))
-const SignIn = lazy(() => import ('./Pages/SignIn'))
-const SignUp = lazy(() => import ('./Pages/SignUp'))
-const ListProperty = lazy(() =>import  ('./Pages/ListProperty'))
-const ForgotPassword = lazy(() => import ('./Pages/ForgotPassword'))
-const Welcome = lazy(() =>import ('./Pages/Welcome'))
-const Profile = lazy(() => import('./Pages/Profile'))
-const PrivateRoute = lazy(() => import ('./components/PrivateRoute'))
-const EditListing = lazy(() => import("./Pages/EditListing"));
-const Blog = lazy(() => import('./Pages/Blog'))
-const ListingItem = lazy(() => import("./Pages/ListingItem"));
-const BookingDetails = lazy(() => import("./components/BookingDetails"));
-const AllListing = lazy(() => import("./Pages/AllListing"));
-  
-
-
-
-
-
+import Help from "./Pages/Help";
+import Home from "./Pages/Home";
+import SignIn from "./Pages/SignIn";
+import SignUp from "./Pages/SignUp";
+import ListProperty from "./Pages/ListProperty";
+import ForgotPassword from "./Pages/ForgotPassword";
+import Welcome from "./Pages/Welcome";
+import Profile from "./Pages/Profile";
+import PrivateRoute from "./components/PrivateRoute";
+import EditListing from "./Pages/EditListing";
+import Blog from "./Pages/Blog";
+import ListingItem from "./Pages/ListingItem";
+import BookingDetails from "./components/BookingDetails";
+import AllListing from "./Pages/AllListing";
 
 function App() {
-  const[showDetails, setShowDetails] = useState(false)
-  
-  function showBookingDetails(){
-    setShowDetails(true)
-    
+  const [showDetails, setShowDetails] = useState(false);
+
+  function showBookingDetails() {
+    setShowDetails(true);
   }
 
-  function hideShowBookingDetails(){
-    setShowDetails(false)
+  function hideShowBookingDetails() {
+    setShowDetails(false);
   }
 
-  
+  return (
+    <>
+      <Router>
+        {showDetails && <BookingDetails onClose={hideShowBookingDetails} />}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/welcome" element={<Welcome />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/listing" element={<AllListing />} />
+          <Route
+            path="/listing/:listingId"
+            element={<ListingItem showBookingDetails={showBookingDetails} />}
+          />
 
-  return <>
-    
-    <Router>
-    {showDetails && <BookingDetails 
-    onClose={hideShowBookingDetails} 
-    
-    />}
-      <Routes>
-        <Route path='/' element={<Home />}/>
-        <Route path='/welcome' element={<Welcome />} />
-        <Route path='/sign-in' element={<SignIn />} />
-        <Route path='/sign-up' element={<SignUp />} />
-        <Route path='/listing' element={<AllListing />} />
-        <Route path='/listing/:listingId' element={<ListingItem 
-        showBookingDetails={showBookingDetails}
-        
-     />} />
-        
-        <Route path='/profile' element={<PrivateRoute/>}>
-        <Route path='/profile' element={<Profile />} />
-        </Route>
-        <Route path='/edit-listing' element={<PrivateRoute/>}>
-        <Route path='/edit-listing/:listingId' element={<EditListing />} />
-        </Route>
-        
-        <Route path='/forgot-password' element={<ForgotPassword />} />
-        <Route path='/help' element={<Help />} />
-        <Route path='/list-property' element={<ListProperty />} />
-        <Route path='/blog/:id' element={<Blog />} />
-      </Routes>
-    </Router>
-    <ToastContainer
+          <Route path="/profile" element={<PrivateRoute />}>
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+          <Route path="/edit-listing" element={<PrivateRoute />}>
+            <Route path="/edit-listing/:listingId" element={<EditListing />} />
+          </Route>
+
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/help" element={<Help />} />
+          <Route path="/list-property" element={<ListProperty />} />
+          <Route path="/blog/:id" element={<Blog />} />
+        </Routes>
+      </Router>
+      <ToastContainer
         position="bottom-center"
         autoClose={5000}
         hideProgressBar={false}
@@ -81,6 +69,7 @@ function App() {
         theme="dark"
       />
     </>
+  );
 }
 
-export default App
+export default App;
